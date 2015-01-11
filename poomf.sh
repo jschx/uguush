1827869b36f57333e1455b8dc4a4456627610800
jsch <me@schil.li>
2015-01-11 16:15:42 -0600

    slim code, use tput, better variables

--- poomf.sh
+++ poomf.sh
@@ -2,22 +2,36 @@
 
 # poomfsh - puush-like functionality for pomf.se
 
+## CONFIGURATION
+
+# Colors
+
+reset=$(tput sgr0)
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+
 ## OPTIONS
 
 # Get options
 while getopts fhsu: option; do
     case $option in
-        f)ful=1;;
-        h)hel=1;;
-        s)sel=1;;
-        u)upl=1;;
+        f)fullscreen=1;;
+        h)help=1;;
+        s)selection=1;;
+        u)upload=1;;
         *)exit;;
     esac
 done
 
 # Fullscreen
-if [[ -z $mul ]]; then
-    if [[ ! -z $ful ]]; then
+if [[ -z $multiple ]]; then
+    if [[ ! -z $fullscreen ]]; then
         # Take fullscreen scrot
         file=$(filename=$(date +%Y-%m-%d)_scrot.png ; maim $filename ; echo -n $filename)
     fi
@@ -27,8 +41,8 @@ else
 fi
 
 # Help
-if [[ -z $mul ]]; then
-    if [[ ! -z $hel ]]; then
+if [[ -z $multiple ]]; then
+    if [[ ! -z $help ]]; then
         # Display help
         echo "-f            fullscreen"
         echo "-h            show this message"
@@ -42,8 +56,8 @@ else
 fi
 
 # Selection
-if [[ -z $mul ]]; then
-    if [[ ! -z $sel ]]; then
+if [[ -z $multiple ]]; then
+    if [[ ! -z $selection ]]; then
         # Take selection scrot
         file=$(filename=$(date +%Y-%m-%d)_scrot.png ; maim -s $filename ; echo -n $filename)
     fi
@@ -53,11 +67,11 @@ else
 fi
 
 # File
-if [[ -z $mul ]]; then
-    if [[ ! -z $upl ]]; then
+if [[ -z $multiple ]]; then
+    if [[ ! -z $upload ]]; then
         # Get file
         file=$(echo $2)
-        mul=1
+        multiple=1
     fi
 else
     echo "error: please only provide one option"
@@ -67,44 +81,38 @@ fi
 ## UPLOADING
 
 # Upload it and grab the url
+
 output=$(curl --silent -sf -F files[]="@$file" "http://pomf.se/upload.php")
-if [[ ! -z $upl ]]; then
-    echo "uploading ${file}..."
-    n=0
-    while [[ $n -le 3 ]]; do
-        printf "\033[37m try #${n}..."
-        if [[ "${output}" =~ '"success":true,' ]]; then
-            pomffile=$(echo "$output" | grep -Eo '"url":"[A-Za-z0-9]+.*",' | sed 's/"url":"//;s/",//')
-            printf '\033[32m done.\n'
-            suc=1
-            break
-        else
-            printf '\033[31m failed.\n'
-            ((n = n +1))
-        fi
-    done
-    url=http://a.pomf.se/$pomffile
-else
-    echo "uploading ${file}..."
-    n=0
-    while [[ $n -le 3 ]]; do
-        printf "\033[37m try #${n}..."
-        if [[ "${output}" =~ '"success":true,' ]]; then
-            pomffile=$(echo "$output" | grep -Eo '"url":"[A-Za-z0-9]+.png",' | sed 's/"url":"//;s/",//')
-            printf '\033[32m done.\n'
-            suc=1
-            break
-        else
-            printf '\033[31m failed.\n'
-            ((n = n +1))
-        fi
-    done
-    url=http://a.pomf.se/$pomffile
-    rm -f $file
+
+echo "uploading ${file}..."
+n=0
+
+while [[ $n -le 3 ]]; do
+  printf "$white try #${n}...$reset"
+  if [[ "${output}" =~ '"success":true,' ]]; then
+    if [[ ! -z $upload ]]; then
+      pomffile=$(echo "$output" | grep -Eo '"url":"[A-Za-z0-9]+.*",' | sed 's/"url":"//;s/",//')
+    else
+      pomffile=$(echo "$output" | grep -Eo '"url":"[A-Za-z0-9]+.png",' | sed 's/"url":"//;s/",//')
+    fi
+    printf "$green done.$reset\n"
+    success=1
+    break
+  else
+    printf "w$red failed.$reset\n"
+    ((n = n +1))
+  fi
+done
+
+url=http://a.pomf.se/$pomffile
+
+if [[ -z $upload ]]; then
+  rm -f $file
 fi
 
 ## OUTPUT
-if [[ ! -z $suc ]]; then
+
+if [[ ! -z $success ]]; then
     # Copy link to clipboard
     echo -n $url | xclip -selection primary
     echo -n $url | xclip -selection clipboard
@@ -113,7 +121,7 @@ if [[ ! -z $suc ]]; then
     # Notify user of completion
     notify-send "pomf!" "$url"
     # Print message to the term
-    echo -e "\033[37m file has been uploaded: $url"
+    printf "$white file has been uploaded: $url$reset\n"
 else
-    echo -e "\033[37m file was not uploaded, did you specify a valid file?"
+    printf "$red file was not uploaded, did you specify a valid file?$reset\n"
 fi
