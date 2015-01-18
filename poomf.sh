c12ae9f704ecfaa0937ea808ca8e10c9273b0dc2
JSchilli1 <jschilli1@live.com>
2015-01-18 16:25:00 -0600

    fix formatting, try printf instead of echo

--- poomf.sh
+++ poomf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# poomfsh - puush-like functionality for pomf.se
+# poomf.sh - puush-like functionality for pomf.se
 
 ## CONFIGURATION
 
-# Colors
+# colors
 
 reset=$(tput sgr0)
 black=$(tput setaf 0)
@@ -18,110 +18,86 @@ white=$(tput setaf 7)
 
 ## OPTIONS
 
-# Get options
+# get options
 while getopts fhsu: option; do
-    case $option in
-        f)fullscreen=1;;
-        h)help=1;;
-        s)selection=1;;
-        u)upload=1;;
-        *)exit;;
-    esac
+	case $option in
+		f)fullscreen=1;;
+		h)help=1;;
+		s)selection=1;;
+		u)upload=1;;
+		*)exit;;
+	esac
 done
 
-# Fullscreen
-if [[ -z $multiple ]]; then
-    if [[ ! -z $fullscreen ]]; then
-        # Take fullscreen scrot
-        file=$(filename=$(date +%Y-%m-%d)_scrot.png ; maim --hidecursor $filename ; echo -n $filename)
-    fi
-else
-    echo "error: please only provide one option"
-    exit
+# take fullscreen picture
+if [[ $fullscreen ]]; then
+	file=$(filename=/tmp/screenshot.png ; maim --hidecursor $filename ; printf $filename)
 fi
 
-# Help
-if [[ -z $multiple ]]; then
-    if [[ ! -z $help ]]; then
-        # Display help
-        echo "-f            fullscreen"
-        echo "-h            show this message"
-        echo "-s            selection"
-        echo "-u file       file upload"
-        exit
-    fi
-else
-    echo "error: please only provide one option"
-    exit
+# display help
+if [[ $help ]]; then
+	printf "%s\t\t%s\n" "-f" "fullscreen"
+	printf "%s\t\t%s\n" "-h" "show this message"
+	printf "%s\t\t%s\n" "-s" "selection"
+	printf "%s\t\t%s\n" "-u file" "file upload"
+	exit
 fi
 
-# Selection
-if [[ -z $multiple ]]; then
-    if [[ ! -z $selection ]]; then
-        # Take selection scrot
-        file=$(filename=$(date +%Y-%m-%d)_scrot.png ; maim -s --hidecursor $filename ; echo -n $filename)
-    fi
-else
-    echo "error: please only provide one option"
-    exit
+# take selection picture
+if [[ $selection ]]; then
+	file=$(filename=/tmp/screenshot.png ; maim -s --hidecursor $filename ; printf $filename)
 fi
 
-# File
-if [[ -z $multiple ]]; then
-    if [[ ! -z $upload ]]; then
-        # Get file
-        file=$(echo $2)
-        multiple=1
-    fi
-else
-    echo "error: please only provide one option"
-    exit
+# get file
+if [[ $upload ]]; then
+	file=$(printf $2)
 fi
 
 ## UPLOADING
 
-# Upload it and grab the url
-
+# upload it and grab the url
 output=$(curl --silent -sf -F files[]="@$file" "http://pomf.se/upload.php")
 
-echo "uploading ${file}..."
-n=0
+printf "%s\n" "uploading ${file}..."
 
+n=0
 while [[ $n -le 3 ]]; do
-  printf "$white try #${n}...$reset"
-  if [[ "${output}" =~ '"success":true,' ]]; then
-    if [[ ! -z $upload ]]; then
-      pomffile=$(echo "$output" | grep -Eo '"url":"[A-Za-z0-9]+.*",' | sed 's/"url":"//;s/",//')
-    else
-      pomffile=$(echo "$output" | grep -Eo '"url":"[A-Za-z0-9]+.png",' | sed 's/"url":"//;s/",//')
-    fi
-    printf "$green done.$reset\n"
-    success=1
-    break
-  else
-    printf "$red failed.$reset\n"
-    ((n = n +1))
-  fi
+	printf "$white try #${n}...$reset"
+	if [[ "${output}" =~ '"success":true,' ]]; then
+		if [[ ! -z $upload ]]; then
+			pomffile=$(printf $output | grep -Eo '"url":"[A-Za-z0-9]+.*",' | sed 's/"url":"//;s/",//')
+		else
+			pomffile=$(printf $output | grep -Eo '"url":"[A-Za-z0-9]+.png",' | sed 's/"url":"//;s/",//')
+		fi
+		printf "$green done.$reset\n"
+		success=1
+		break
+	else
+		printf "$red failed.$reset\n"
+		((n = n +1))
+	fi
 done
 
 url=http://a.pomf.se/$pomffile
 
+# remove temporary file
+
 if [[ -z $upload ]]; then
-  rm -f $file
+	rm -f $file
 fi
 
 ## OUTPUT
 
-if [[ ! -z $success ]]; then
-    # Copy link to clipboard
-    echo -n $url | xclip -selection primary
-    echo -n $url | xclip -selection clipboard
-    # Log url to file
-    echo $url "<<" "$(date +"[%D] | [%H:%M:%S]")" >> ~/.pomfs.txt
-    # Notify user of completion
-    notify-send "pomf!" "$url"
-    # Print message to the term
-    printf "file has been uploaded: $url\n"
+if [[ $success ]]; then
+	# copy link to clipboard
+	printf $url | xclip -selection primary
+	printf $url | xclip -selection clipboard
+	# log url to file
+	echo "$(date +"%D %H:%M:%S") | $url" >> ~/.pomfs.txt
+	# notify user of completion
+	notify-send "pomf!" "$url"
+	# output message to term
+	printf "%s\n" "file has been uploaded: $url"
 else
-    printf "file was not uploaded, did you specify a valid filename?\n"
+	printf "%s\n" "file was not uploaded, did you specify a valid filename?"
 fi
