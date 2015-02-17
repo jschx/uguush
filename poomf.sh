aa53851786bbe4f4ff19ac056401b9def59653b6
Joe Schillinger <js1@openmailbox.org>
2015-02-17 17:25:35 -0600

    Standardize variable formatting, other formatting fixes.

--- poomf.sh
+++ poomf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # poomf.sh - puush-like functionality for pomf.se and uguu.se
 #
@@ -14,13 +14,13 @@ R=$(tput setaf 1)
 G=$(tput setaf 2)
 
 # Default screenshot name.
-FILE="/tmp/screenshot.png"
+FILE='/tmp/screenshot.png'
 
 
 ## FUNCTIONS
 function maim {
-	command maim --hidecursor $@
-	(( "$?" != 0 )) && exit 1
+	command maim --hidecursor "${@}"
+	(( "${?}" != 0 )) && exit 1
 }
 
 function usage {
@@ -28,7 +28,7 @@ function usage {
 	poomf.sh - puush-like functionality for pomf.se and uguu.se
 
 	Usage:
-	    $(basename $0) [options]
+	    $(basename "${0}") [options]
 
 	Options:
 	    -h         Show this help message.
@@ -41,23 +41,26 @@ function usage {
 }
 
 ## EXIT IF NO ARGUMENTS FOUND
-test -z $1 && { usage && exit 1; }
+if [[ -z "${1}" ]]; then
+	usage
+	exit 1
+fi
 
 ## PARSE OPTIONS
 while getopts :fghsu: opt; do
-	case $opt in
+	case "${opt}" in
 		f)
 			# Take shot.
-			maim "$FILE" ;;
+			maim "${FILE}" ;;
 		g)
 			# Change mode to uguu
 			uguu=1 ;;
 		s)
 			# Take shot with selection.
-			maim -s "$FILE" ;;
+			maim -s "${FILE}" ;;
 		u)
 			# Change $FILE to the specified file with -u
-			FILE="$OPTARG" ;;
+			FILE="${OPTARG}" ;;
 		h)
 			# Show help and exit with EXIT_SUCCESS
 			usage && exit 0 ;;
@@ -69,30 +72,30 @@ done
 
 ## UPLOAD FILE
 for (( i = 1; i <= 3; i++ )); do
-	echo -n "Try #${i} ... "
+	echo -n "Try #${i}... "
 
 	# Upload file to selected host
-	if [[ "$uguu" ]]; then
-		pomf=$(curl -sf -F file="@$FILE" "http://uguu.se/api.php?d=upload")
+	if [[ "${uguu}" ]]; then
+		pomf=$(curl -sf -F file="@${FILE}" "http://uguu.se/api.php?d=upload")
 	else
-		pomf=$(curl -sf -F files[]="@$FILE" "http://pomf.se/upload.php?output=gyazo")
+		pomf=$(curl -sf -F files[]="@${FILE}" "http://pomf.se/upload.php?output=gyazo")
 	fi
 
-	if (( "$?" == 0 )); then
+	if (( "${?}" == 0 )); then
 
 		# Copy link to clipboard
-		echo -n "$pomf" | xclip -selection primary
-		echo -n "$pomf" | xclip -selection clipboard
+		echo -n "${pomf}" | xclip -selection primary
+		echo -n "${pomf}" | xclip -selection clipboard
 
 		# Log url to file
-		echo "$(date +"%D %R") | $pomf" >> ~/.pomfs.txt
+		echo "$(date +"%D %R") | ${pomf}" >> ~/.pomfs.txt
 
 		# Notify user of completion
-		notify-send "pomf!" "$pomf"
+		notify-send "pomf!" "${pomf}"
 
 		# Output message to term
 		echo "[${G}OK${N}]"
-		echo "File has been uploaded: $pomf"
+		echo "File has been uploaded: ${pomf}"
 		exit
 	else
 		echo "[${R}FAILED${N}]"
